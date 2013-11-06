a679bfdbd681dbd6f1369a371e1279780e758d05
Joel Williamson <joel.s.williamson@gmail.com>
2013-11-05 21:52:17 -0500

    Fixed errors with bracketing

--- linked_list.h
+++ linked_list.h
@@ -1,6 +1,6 @@
 /* Return a pointer to the object containing a field */
 #define container_of(ptr_to_field,object_name,field_name) \
-	(object_name*)(((void*)ptr_to_field) - (void*)&(((object_name*)0)->field_name))
+	((object_name*)(((void*)ptr_to_field) - (void*)&(((object_name*)0)->field_name)))
 
 struct list_node {
 	struct list_node * next;	/* The next node in the list, NULL if this node is last */
@@ -13,10 +13,10 @@ struct list_node {
  * list_name is the name of the list_node field in each element
  */
 #define next_element(current_element,element_type,list_name) \
-	container_of(current_element.list_name.next,element_type,list_name.next)
+	(container_of((current_element)->list_name.next,element_type,list_name))
 
 #define prev_element(current_element,element_type,list_name) \
-	container_of(current_element.list_name.prev,element_type,list_name.prev)
+	(container_of((current_element)->list_name.prev,element_type,list_name.prev))
 
 struct list_node * find_last_node(struct list_node * current_node);
 
@@ -24,7 +24,7 @@ struct list_node * find_last_node(struct list_node * current_node);
  * Return a pointer to the last element in the list
  */
 #define find_last(current_element,element_type,list_name) \
-	container_of(find_last_node(current_element.list_name),element_type,list_name)
+	(container_of(find_last_node((current_element)->list_name),element_type,list_name))
 
 struct list_node * find_prev_node(struct list_node * current_node);
 
@@ -32,11 +32,11 @@ struct list_node * find_prev_node(struct list_node * current_node);
  * Return a pointer to the first element in the list
  */
 #define find_first(current_element,element_type,list_name) \
-	container_of(find_first_node(current_element.list_name),element_type,list_name)
+	(container_of(find_first_node((current_element)->list_name),element_type,list_name))
 
 /*
  * Insert the new element after the last element and return a pointer to it
  * Requires a pointer to the current element
  */
 #define append(current_element,element_type,list_name,ptr_to_new_element) \
-	find_last_node(current_element.list_name).list_name.next = ptr_to_new_element
+	(find_last_node(&(current_element)->list_name)->next = &(ptr_to_new_element->list_name))
