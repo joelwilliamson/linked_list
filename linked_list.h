4df5a2e699fb85f917e8ebf19777f83d57e7db34
Joel Williamson <joel.s.williamson@gmail.com>
2013-11-05 21:06:23 -0500

    Reorded the argument order for container_of

--- linked_list.h
+++ linked_list.h
@@ -1,5 +1,5 @@
 /* Return a pointer to the object containing a field */
-#define container_of(ptr_to_field,field_name,object_name) \
+#define container_of(ptr_to_field,object_name,field_name) \
 	(object_name*)(((void*)ptr_to_field) - (void*)&(((object_name*)0)->field_name))
 
 struct list_node {
@@ -13,10 +13,10 @@ struct list_node {
  * list_name is the name of the list_node field in each element
  */
 #define next_element(current_element,element_type,list_name) \
-	container_of(current_element.list_name.next,list_name.next,element_type)
+	container_of(current_element.list_name.next,element_type,list_name.next)
 
 #define prev_element(current_element,element_type,list_name) \
-	container_of(current_element.list_name.prev,list_name.prev,element_type)
+	container_of(current_element.list_name.prev,element_type,list_name.prev)
 
 struct list_node * find_last_node(struct list_node * current_node);
 
@@ -24,7 +24,7 @@ struct list_node * find_last_node(struct list_node * current_node);
  * Return a pointer to the last element in the list
  */
 #define find_last(current_element,element_type,list_name) \
-	container_of(find_last_node(current_element.list_name),list_name,element_type)
+	container_of(find_last_node(current_element.list_name),element_type,list_name)
 
 struct list_node * find_prev_node(struct list_node * current_node);
 
@@ -32,4 +32,4 @@ struct list_node * find_prev_node(struct list_node * current_node);
  * Return a pointer to the first element in the list
  */
 #define find_first(current_element,element_type,list_name) \
-	container_of(find_first_node(current_element.list_name),list_name,element_type)
+	container_of(find_first_node(current_element.list_name),element_type,list_name)
